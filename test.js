9cbcfbcb103b97c414e6e24dc88f05956b333107
sergiy-smaga <seregasmaga@gmail.com>
2022-07-15 18:38:53 +0300

    1st task done

--- test.js
+++ test.js
@@ -1,33 +1,18 @@
-function makeObjectDeepCopy(obj) {
-  if (typeof obj !== "object") {
-    return;
+Array.prototype.myFilter = function (cb, obj) {
+  let result = [];
+  let context = this;
+
+  if (obj) {
+    context = obj;
   }
-  let copy = {};
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      if (typeof obj[key] === "object") {
-        copy[key] = { ...obj[key] };
-      } else {
-        copy[key] = obj[key];
+
+  for (let i = 0; i < context.length; i++) {
+    if (i in context) {
+      el = context[i];
+      if (cb.call(context, el, i, context)) {
+        result.push(el);
       }
     }
   }
-  return copy;
-}
-
-function selectFromInterval(arr, num1, num2) {
-  if (!Array.isArray(arr) || isNaN(num1) || isNaN(num2)) {
-    throw new Error(`Give me array and two numbers`);
-  }
-
-  let min = num1 < num2 ? num1 : num2;
-  let max = num1 < num2 ? num2 : num1;
-
-  return arr.filter((el) => {
-    if (typeof el !== "number") {
-      throw new Error("Array should contain only numbers");
-    } else {
-      return el >= min && el <= max;
-    }
-  });
-}
+  return result;
+};
